28f5e68254eca1a98c31ff05634fa09bc32b8bb2
student <student@PC111S14.fgakusei.local>
2014-05-26 11:05:02 +0900

    a

--- src/com/example/hellogalaxy/MainActivity.java
+++ src/com/example/hellogalaxy/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.hellogalaxy;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+		Button btnOK = (Button)findViewById(R.id.btnOK);
+		btnOK.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v){
+		switch(v.getId()){
+		case R.id.btnOK:
+			EditText etv = (EditText)findViewById(R.id.edtFirst);
+			 String inputMsg = etv.getText().toString();
+			EditText etv2 = (EditText)findViewById(R.id.edtFamily);
+			 String inputMsg2 = etv2.getText().toString();
+			 
+			 //メッセージ表示用のTextViewを探して、文字を設定
+			 TextView tv = (TextView)findViewById(R.id.txvMsg);
+			 tv.setText("あなたでしたか、" + inputMsg + inputMsg2 + "さん。"); 
+		}
+		
+
+		
+	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
